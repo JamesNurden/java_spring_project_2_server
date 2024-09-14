99b215c33615c8530dfa75c9cf351bf1b8b8b723
James <jnurden@outlook.com>
2024-09-14 12:35:08 +0100

    Your commit message

---
+++ src/test/java/com/dailycodework/main/java/com/dailycodework/lakesidehotel/exception/UserAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package com.dailycodework.lakesidehotel.exception;
+
+/**
+ * @author Simpson Alfred
+ */
+
+public class UserAlreadyExistsException extends RuntimeException{
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
